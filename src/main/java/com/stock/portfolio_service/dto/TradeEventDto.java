e33b831b9cada2feb9d6ce96df4348d40caaddc2
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2026-05-02 00:13:17 +0530

    Refactor TradeEventDto for cleaner code
    Refactored TradeEventDto by replacing Lombok annotations with @Data and modifying field names.

--- src/main/java/com/stock/portfolio_service/dto/TradeEventDto.java
+++ src/main/java/com/stock/portfolio_service/dto/TradeEventDto.java
@@ -1,28 +1,19 @@
 package com.stock.portfolio_service.dto;
 
-import lombok.*;
+import lombok.Data;
 
-import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class TradeEventDto implements Serializable {
-
+@Data
+public class TradeEventDto {
     private Long tradeId;
     private Long buyOrderId;
     private Long sellOrderId;
-
     private Long buyerUserId;
     private Long sellerUserId;
-
     private String stockSymbol;
-    private Integer quantity;
-    private BigDecimal executedPrice;
-
+    private int quantity;
+    private BigDecimal price;
     private LocalDateTime executionTime;
 }
